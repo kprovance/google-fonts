d33a2a39fe87517eb2066b1f96e3e8c77e3f2104
Kevin Provance <kevin.provance@gmail.com>
2017-12-13 19:56:22 -0500

    Update run.php

--- run.php
+++ run.php
@@ -132,6 +132,7 @@
     $build_number    = getenv( 'TRAVIS_BUILD_NUMBER' );
     echo shell_exec( "git commit -m \"Travis build: $build_number [skip ci]\"" );
     $gh_token = getenv( 'GH_TOKEN' );
+    echo $gh_token;
     echo shell_exec( "git remote set-url origin https://$gh_token@github.com/reduxframework/google-fonts.git > /dev/null 2>&1" );
     echo "\n\n";
     //echo shell_exec("git push https://$gh_token@github.com/reduxframework/google-fonts.git --force");
